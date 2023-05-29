a68f8e18beff677d334cc247a3ba6394a3599fa2
Eldar <EldarK@outlook.com>
2023-05-29 21:45:07 +0300

    birds are switching

---
+++ app/src/main/java/com/example/listofbirds/Bird.java
@@ -0,0 +1,13 @@
+package com.example.listofbirds;
+
+public class Bird {
+    String name, url, sound;
+    int image;
+
+    public Bird(String name, String url, String sound, int image) {
+        this.name = name;
+        this.url = url;
+        this.sound = sound;
+        this.image = image;
+    }
+}
--- app/src/main/java/com/example/listofbirds/MainActivity.java
+++ app/src/main/java/com/example/listofbirds/MainActivity.java
@@ -1,15 +1,85 @@
 package com.example.listofbirds;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView birdPicture;
+    TextView birdName;
+
+    static int currentBird = 0;
+    Bird[] birds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        birds = loadBirds();
+
+        birdPicture = findViewById(R.id.bird_image);
+        birdName = findViewById(R.id.bird_name);
+
+        updateBird();
+    }
+
+    public void onPictureClick(View view) {
+
+    }
+
+    public void onPreviousClick(View view) {
+        currentBird--;
+        updateBird();
+    }
+
+    public void onNextClick(View view) {
+        currentBird++;
+        updateBird();
+    }
+
+    public void onHelpClick(View view) {
+        String url = birds[currentBird % birds.length].url;
+
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+    }
+
+    void updateBird() {
+        birdName.setText(birds[currentBird % birds.length].name);
+        birdPicture.setImageResource(birds[currentBird % birds.length].image);
+    }
+
+    Bird[] loadBirds() {
+        if (birds != null) {
+            return birds;
+        }
+
+        String[] birdsNames = getResources().getStringArray(R.array.birds_names);
+        TypedArray birdsPictures = getResources().obtainTypedArray(R.array.birds_images);
+
+        String[] birdsURLs = getResources().getStringArray(R.array.birds_urls);
+
+        birds = new Bird[birdsNames.length];
+
+        for (int i = 0; i < birds.length; i++) {
+            birds[i] = new Bird(birdsNames[i], birdsURLs[i], null, birdsPictures.getResourceId(i, 0));
+        }
+
+        birdsPictures.recycle();
+
+        return birds;
     }
 }
